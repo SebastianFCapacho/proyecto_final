b9bde7bd7f2843fec4548bec34a1ac3d8b5ded3d
Mateomr-Dg <mateodagua878@gmail.com>
2025-12-11 21:06:25 -0500

    holll

--- Program.cs
+++ Program.cs
@@ -1,26 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
+﻿using System;
 using System.Globalization;
-
+// holaaaa
 namespace SistemaNotas
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Vectores de estudiantes y materias
+        
             string[] estudiantes = { "Ana", "Luis", "Sofía" };
             string[] materias = { "Matemáticas", "Inglés", "Programación" };
 
             int cantidadEstudiantes = estudiantes.Length;
             int cantidadMaterias = materias.Length;
 
-            // Matriz de notas: filas = estudiantes, columnas = materias
             double[,] notas = new double[cantidadEstudiantes, cantidadMaterias];
 
             Console.WriteLine("=== SISTEMA DE NOTAS (VECTORES + MATRIZ) ===\n");
 
-            // 1. Ingreso de notas
             Console.WriteLine("INGRESO DE NOTAS:");
             for (int i = 0; i < cantidadEstudiantes; i++)
             {
@@ -34,7 +31,6 @@ namespace SistemaNotas
                         Console.Write($"Ingrese la nota de {estudiantes[i]} en {materias[j]} (0.0 - 5.0): ");
                         string entrada = Console.ReadLine();
 
-                        // Permitir coma o punto como separador decimal
                         entrada = entrada.Replace(',', '.');
 
                         entradaCorrecta = double.TryParse(
@@ -58,7 +54,6 @@ namespace SistemaNotas
                 Console.WriteLine();
             }
 
-            // 2. Mostrar la tabla completa de notas
             Console.WriteLine("\n=== TABLA DE NOTAS ===");
             Console.Write("Estudiante\\Materia\t");
             for (int j = 0; j < cantidadMaterias; j++)
@@ -77,7 +72,6 @@ namespace SistemaNotas
                 Console.WriteLine();
             }
 
-            // 3. Promedio por estudiante (fila)
             Console.WriteLine("\n=== PROMEDIO POR ESTUDIANTE ===");
             for (int i = 0; i < cantidadEstudiantes; i++)
             {
@@ -90,7 +84,6 @@ namespace SistemaNotas
                 Console.WriteLine($"Promedio de {estudiantes[i]}: {promedio.ToString("0.00", CultureInfo.InvariantCulture)}");
             }
 
-            // 4. Promedio por materia (columna)
             Console.WriteLine("\n=== PROMEDIO POR MATERIA ===");
             for (int j = 0; j < cantidadMaterias; j++)
             {
